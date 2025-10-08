3152ef300778ba01f71903e02fcad7e9692a0e11
Arda Nakisci <anakisci@gmail.com>
2025-10-08 16:37:31 +0300

    only use release link

--- src/constants.rs
+++ src/constants.rs
@@ -4,9 +4,6 @@ pub const CLI_ARCHIVE_NAME: &str = "rain-orderbook-cli.tar.gz";
 pub const CLI_ARCHIVE_URL_TEMPLATE: &str = "https://raw.githubusercontent.com/rainlanguage/rain.orderbook/{commit}/crates/cli/bin/rain-orderbook-cli.tar.gz";
 pub const RELEASE_DOWNLOAD_URL_TEMPLATE: &str =
     "https://github.com/findolor/local_db_remote/releases/latest/download/{file}";
-pub const RELEASE_DOWNLOAD_URL_BY_TAG_TEMPLATE: &str =
-    "https://github.com/findolor/local_db_remote/releases/download/{tag}/{file}";
-
 pub const API_TOKEN_ENV_VARS: &[&str] = &[
     "RAIN_API_TOKEN",
     "RAIN_ORDERBOOK_API_TOKEN",
